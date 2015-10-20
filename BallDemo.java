3acb9911043ade8bab59527c4c1c880d25a9203f
Jennifer Puhalka <g00230799@W213-28.academic.raritanval.edu>
2015-10-19 21:14:29 -0400

    Working Copy

--- BallDemo.java
+++ BallDemo.java
@@ -31,6 +31,7 @@ public class BallDemo
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
+        
 
         // crate and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
@@ -48,6 +49,40 @@ public class BallDemo
             if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
                 finished = true;
             }
+        }
+    }
+    
+    public void boxBounce()
+    {
+        int ground = 100;   // position of the ground line
+        int top = 800;
+        int right = 550;
+        int left = 50;
+
+        myCanvas.setVisible(true);
+
+        // draw the ground
+        myCanvas.drawLine(left, ground, right, ground);
+        // draw the top
+        myCanvas.drawLine(left, top, right, top);
+        //draw the left side
+        myCanvas.drawLine(left, ground, left, top);
+        //draw the right side
+        myCanvas.drawLine(right, ground, right, top);
+
+        // create and show the balls
+        BoxBall ball = new BoxBall(50, 50, 16, Color.BLUE, ground, top, right, left, myCanvas);
+        ball.draw();
+        BoxBall ball2 = new BoxBall(70, 80, 20, Color.RED, ground, top, right, left, myCanvas);
+        ball2.draw();
+
+        // make them bounce
+        boolean finished =  false;
+        while(!finished) 
+        {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
         }
     }
 }
